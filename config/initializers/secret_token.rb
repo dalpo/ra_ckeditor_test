652a4f217421cdc13bcf42e1fda743658ee010ee
Andrea Dal Ponte <dalpo85@gmail.com>
2013-01-20 15:42:18 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaCkeditorTest::Application.config.secret_token = '2ccfe22821fe3e4c94838349b3e47a02fea6c03fde99718be44f2d6e79778ba543d28ec49cb80ab597a9a40603aa3d8cd87b26bd6a43db736c47ef3e3c089b55'
